97120876ad11ab02d66f3dcf28bf55278f1a7b4b
Hottory <gkffndnl123@naver.com>
2018-12-29 12:16:50 +0900

    Initial commit

---
+++ app/src/main/java/kr/tjeit/startactivityforresulttest/BaseActivity.java
@@ -0,0 +1,15 @@
+package kr.tjeit.startactivityforresulttest;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity{
+
+    public Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setvalues();
+    public abstract void bindviews
+            ();
+
+}
